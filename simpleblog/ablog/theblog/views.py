35c168b5ec6c16830f0d8a598b09587b99ec7f03
branyakov <115071601+branyakov@users.noreply.github.com>
2022-10-16 23:59:11 +0300

    blog/add post

--- simpleblog/ablog/theblog/views.py
+++ simpleblog/ablog/theblog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+# from django.shortcuts import render
+from django.views.generic import CreateView, DetailView, ListView
+
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -15,3 +17,10 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = "article_details.html"
+    
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
