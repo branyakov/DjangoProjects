43703f7c532b4aa28917d263584f20a373437a72
branyakov <115071601+branyakov@users.noreply.github.com>
2022-10-17 14:59:15 +0300

    blog/add staff

--- simpleblog/ablog/theblog/views.py
+++ simpleblog/ablog/theblog/views.py
@@ -1,8 +1,11 @@
 # from django.shortcuts import render
-from django.views.generic import CreateView, DetailView, ListView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
-from .models import Post
-from .forms import PostForm
+from .models import *
+from .forms import PostForm, EditForm
+
 
 # Create your views here.
 
@@ -12,15 +15,41 @@ from .forms import PostForm
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    
-    
+    ordering = ['post_date']
+
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats)
+    return render(request, 'categories.html', {'cats': cats, 'category_posts': category_posts})
+
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = "article_details.html"
-    
+
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    # form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
+
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'title_tag', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
