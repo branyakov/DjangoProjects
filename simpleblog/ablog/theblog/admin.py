b44b9c901bc3acd4786347dbe99097d834fbc033
branyakov <115071601+branyakov@users.noreply.github.com>
2022-12-18 04:54:01 +0200

    update

--- simpleblog/ablog/theblog/admin.py
+++ simpleblog/ablog/theblog/admin.py
@@ -1,10 +1,42 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import *
 
 # Register your models here.
 
-admin.site.register(Post)
-admin.site.register(Category)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'header_image', 'title_tag', 'author', 'post_date', 'category', 'snippet')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'body')
+    list_filter = ('category', 'author')
+    save_on_top = True
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    list_display_links = ('id', 'email')
+    search_fields = ('name',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date_added')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'date_added')
+    list_filter = ('name',)
+
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Profile)
-admin.site.register(Comment)
-admin.site.register(Contact)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Contact, ContactAdmin)
